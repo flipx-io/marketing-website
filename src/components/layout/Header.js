1fbc6a83f38f4361bd0d47d4d6087a4b69651257
mrspanishviking <cardenas88karl@gmail.com>
2022-02-24 19:19:56 -0700

    fix: added logo and urls to main app

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 import Logo from './partials/Logo';
 import Button from '../elements/Button';
 
