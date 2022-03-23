48b6c49048f75a8071a1b1f5604aae1f2e2ebff8
Charlie Paik <chpaik88@gmail.com>
2022-03-22 18:12:57 -0700

    ci: initialize puppeteer

---
+++ test/features/hooks.js
@@ -0,0 +1,21 @@
+// // Dependencies
+// const { After, Before, AfterAll } = require('@cucumber/cucumber');
+// const scope = require('./support/scope');
+
+// Before(async () => {
+// });
+
+// After(async () => {
+//   if (scope.browser && scope.context.currentPage) {
+//     const cookies = await scope.context.currentPage.cookies();
+//     if (cookies && cookies.length > 0) {
+//       await scope.context.currentPage.deleteCookie(...cookies);
+//     }
+//     await scope.context.currentPage.close();
+//     scope.context.currentPage = null;
+//   }
+// });
+
+// AfterAll(async () => {
+//   if (scope.browser) await scope.browser.close();
+// });
---
+++ test/features/step_definitions/steps.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+const helpers = require("./helpers")
+const puppeteer = require('puppeteer');
+const scope = require('../support/scope');
+
+setDefaultTimeout(60*1000);
+
+const initBrowser = async() => {
+  let browser = await puppeteer.launch({
+    args: ['--no-sandbox'],
+    executablePath: process.env.CI === true ? process.env.PUPPETEER_EXEC_PATH : "",
+    headless: true,
+    devtools: false
+  });
+
+}
+
+Given('I am on the homepage', visitHomePage);
+
+// Then('I should be on the {string} page', shouldBeOnPage);
