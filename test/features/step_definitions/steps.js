5d27af535901c36c5499b1d7dc699c3918814901
Charlie Paik <chpaik88@gmail.com>
2022-03-22 19:01:53 -0700

    ci: headless = true

--- test/features/step_definitions/steps.js
+++ test/features/step_definitions/steps.js
@@ -10,7 +10,7 @@ const initBrowser = async() => {
   let browser = await puppeteer.launch({
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
     executablePath: process.env.CI === true ? process.env.PUPPETEER_EXEC_PATH : "",
-    headless: process.env.CI === true ? true : false,
+    headless: false,
     devtools: false
   });
 
